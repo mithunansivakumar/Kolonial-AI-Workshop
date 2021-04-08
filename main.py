60e461a3147d7ffb7d868cfeff082a29278fa287
tordstr <tordstr@stud.ntnu.no>
2021-04-08 18:25:40 +0200

    ferdig

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from numpy import mean
 
 tf.__version__
 
@@ -10,16 +11,19 @@ tf.__version__
 # Importing the dataset
 dataset = pd.read_csv('deliveries-2.csv')
 print(dataset.columns)
-dataset.drop(labels=['delivery_date', 'slot_start_time', 'slot_end_time'], axis=1, inplace=True)
+dataset.drop(labels=['delivery_date', 'slot_start_time', 'slot_end_time', 'street', 'street_number', 'zip_code', 'zip_place', 'area_segment', 'municipality'], axis=1, inplace=True)
+#, 'street', 'street_number', 'zip_code','zip_place', 'area_segment', 'municipality'
+dataset.dropna(inplace=True)
 print(dataset.columns) # can_select_unattend
 
 # Features
 X = dataset.iloc[:, 1:-1].values
 
 
-
+from sklearn import preprocessing
 # Service time
 y = dataset.iloc[:, -1].values
+
 print("---X---")
 print(X)
 print("---Y---")
@@ -35,21 +39,20 @@ X[:, 3] = le.fit_transform(X[:, 3])
 X[:, 4] = le.fit_transform(X[:, 4])
 
 
+print("-------------")
+print(X[0])
+
+
 # One Hot Encoding the "non-binary" columns
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [10])], remainder='passthrough')
-X = np.array(ct.fit_transform(X))
-
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [12])], remainder='passthrough')
-X = np.array(ct.fit_transform(X))
-
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [13])], remainder='passthrough')
-X = np.array(ct.fit_transform(X))
-
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [14])], remainder='passthrough')
+"""
+print(type(X[0][9]))
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [9, 11, 12, 13, 14])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 print(X)
+"""
+
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -74,18 +77,26 @@ ann = tf.keras.models.Sequential()
 # The dense layer is found to be the most commonly used layer in the models.
 
 # Adding the input layer and the first hidden layer
-ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=64, activation='relu'))
 
 # Adding the second hidden layer
-ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=128, activation='relu'))
 
 # Adding the output layer
-ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+ann.add(tf.keras.layers.Dense(units=1, activation='relu'))
 
 # Training the ANN
 
 # Compiling the ANN
-ann.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+ann.compile(optimizer='adam', loss='MAE', metrics=['MAE'])
 
 # Training the ANN on the Training set
-ann.fit(X_train, y_train, batch_size=32, epochs=20)
+ann.fit(X_train, y_train, batch_size=64, epochs=50)
+
+preds = ann.predict(X_test)
+
+value = tf.keras.losses.MAE(
+    y_test, preds
+)
+
+print(mean(value))
